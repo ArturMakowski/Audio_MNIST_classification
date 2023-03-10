7baec91537515ad5eafd19c00c9e5b761052688e
Artur Makowski <artmak1@st.amu.edu.pl>
2023-03-10 21:54:19 +0100

    Performed data preprocessing tasks

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import os 
 
 import matplotlib.pyplot as plt
+import torch
 from torch.utils.data import Dataset
 import torchaudio
 import pandas as pd
@@ -12,11 +13,13 @@ class AudioMNISTDataset(Dataset):
         audio_dir_path (str): Path to the directory containing the audio files.    
     """
 
-    def __init__(self, audio_dir_path):
+    def __init__(self, audio_dir_path, transformation, num_samples_per_clip=48000):
 
         """Initializes the dataset."""
 
         self.audio_dir_path = audio_dir_path
+        self.transformation = transformation
+        self.num_samples_per_clip = num_samples_per_clip
         self.file_list = []
         self.label_list = []
         for root, _, files in os.walk(self.audio_dir_path):
@@ -36,32 +39,65 @@ class AudioMNISTDataset(Dataset):
         """Returns a tuple (signal, label, file_path)"""
 
         signal, sr = torchaudio.load(self.file_list[index])
-        return signal, self.label_list[index], self.file_list[index]
+        signal = self._right_zero_pad(signal)
+        signal_transformed = self.transformation(signal)
+        return signal,signal_transformed, sr, self.label_list[index], self.file_list[index]
     
-def plot_distribution_of_audio_file_lengths(audio_mnist):
+    def _right_zero_pad(self, signal):
+        length_signal = signal.shape[1]
+        if length_signal < self.num_samples_per_clip:
+            num_missing_samples = self.num_samples_per_clip - length_signal
+            signal = torch.nn.functional.pad(signal, (0, num_missing_samples))
+        return signal
+    
+
+
+def plot_distribution_of_audio_lengths(audio_mnist):
     audio_file_lengths = []
+    sample_rates = []
+
     for i in range(len(audio_mnist)):
-        signal, _, _ = audio_mnist[i]
+        signal, _, sr, _, _ = audio_mnist[i]
         audio_file_lengths.append(signal.shape[1])
-    plt.hist(audio_file_lengths, bins=100)
-    plt.title("Distribution of audio file lengths")
-    plt.xlabel("Audio file length")
-    plt.ylabel("Number of audio files")
-    plt.show()
+        sample_rates.append(sr)
+
+    print(f"Max audio file length: {max(audio_file_lengths)} samples and {max(audio_file_lengths)/48000} seconds")
+    fig = plt.figure(figsize=(13, 10))
+    ax1 = fig.add_subplot(3, 1, 1)
+    ax1.hist(audio_file_lengths, bins=100)
+    ax1.set_title("Distribution of audio file lengths")
+    ax1.set_xlabel("Audio file length")
+    ax1.set_ylabel("Number of audio files")
+
+    # ax2 = fig.add_subplot(3, 1, 2)
+    # ax2.hist(sample_rates, bins=100)
+    # ax2.set_title("Distribution of sample rates")
+    # ax2.set_xlabel("Sample rate")
+    # ax2.set_ylabel("Number of audio files")
+
 
 if __name__ == "__main__":
 
     # Test the dataset
 
-    audio_dir_path = "/home/armak/Python_projects_WSL/Audio_MNIST_classification/data"
+    AUDIO_DIR_PATH = "/home/armak/Python_projects_WSL/Audio_MNIST_classification/data"
+    SAMPLE_RATE = 48000
 
-    audio_mnist = AudioMNISTDataset(audio_dir_path)
+    mel_spectrogram_transformation = torchaudio.transforms.MelSpectrogram(
+        sample_rate=SAMPLE_RATE,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=64
+    )
+
+    audio_mnist = AudioMNISTDataset(AUDIO_DIR_PATH, mel_spectrogram_transformation)
 
     print(f"There are {len(audio_mnist)} samples in the dataset.")
 
-    signal, label, file_path = audio_mnist[0]
+    signal, signal_transformed, sr, label, file_path = audio_mnist[0]
 
     print(f'{signal.shape}, {label}, {file_path}')
 
-    # Plot the distribution of the audio file lengths
-    # plot_distribution_of_audio_file_lengths(audio_mnist)
+    # Plot the distribution of the audio file lengths and sample rates
+    # plot_distribution_of_audio_lengths(audio_mnist)
+
